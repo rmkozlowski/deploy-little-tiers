d09d1b2e56b975dc9c0e7de8fd860ccc22b6a570
Rachell Kozlowski <120128966+rmkozlowski@users.noreply.github.com>
2023-07-25 15:24:30 -0400

    Update http_server.js

--- http_server.js
+++ http_server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const app     = express();
-const low     = require('lowdb');
-const fs      = require('lowdb/adapters/FileSync');
+const app = express();
+const low = require('lowdb');
+const fs = require('lowdb/adapters/FileSync');
 const adapter = new fs('db.json');
-const db      = low(adapter);
-const cors    = require('cors');
+const db = low(adapter);
+const cors = require('cors');
 const { faker } = require('@faker-js/faker');
 
 // allow cross-origin resource sharing (CORS)
@@ -20,27 +20,54 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // init the data store
-db.defaults({ users: []}).write();
+db.defaults({ users: [] }).write();
+
+let port = process.env.PORT || 3001;
 
 // return all users
-app.get('/data', function(req, res){     
+app.get('/data', function (req, res) {
     res.send(db.get('users').value());
 });
 
 // add user
-app.post('/add', function(req, res){
+app.post('/add', function (req, res) {
     var user = {
-        'name'          : req.body.name,
-        'dob'           : req.body.dob,
-        'email'         : req.body.email,
-        'username'      : req.body.username,
-        'password'      : req.body.password,
-        'phone'         : req.body.phone,
-        'streetaddress' : req.body.streetaddress,
-        'citystatezip'  : req.body.citystatezip,
-        'latitude'      : req.body.latitude,
-        'longitude'     : req.body.longitude,
-        'avatar'        : faker.internet.avatar() 
+        'name': req.body.name,
+        'dob': req.body.dob,
+        'email': req.body.email,
+        'username': req.body.username,
+        'password': req.body.password,
+        'phone': req.body.phone,
+        'streetaddress': req.body.streetaddress,
+        'citystatezip': req.body.citystatezip,
+        'latitude': req.body.latitude,
+        'longitude': req.body.longitude,
+        'avatar': faker.internet.avatar() 
+    }
+    db.get('users').push(user).write();
+    console.log(db.get('users').value());
+    res.send(db.get('users').value());
+});
+
+// get "account"
+app.get('/accounts', function (req, res) {
+    res.send(db.get('users').value());
+});
+
+//post "account"
+app.post('/accounts', function (req, res) {
+    let user = {
+        'name': req.body.name,
+        'dob': req.body.dob,
+        'email': req.body.email,
+        'username': req.body.username,
+        'password': req.body.password,
+        'phone': req.body.phone,
+        'streetaddress': req.body.streetaddress,
+        'citystatezip': req.body.citystatezip,
+        'latitude': req.body.latitude,
+        'longitude': req.body.longitude,
+        'avatar': faker.internet.avatar()
     }
     db.get('users').push(user).write();
     console.log(db.get('users').value());
@@ -49,3 +76,6 @@ app.post('/add', function(req, res){
 
 // start server
 // -----------------------
+app.listen(port, function () {
+    console.log(`Running on port ${port}`);
+});
